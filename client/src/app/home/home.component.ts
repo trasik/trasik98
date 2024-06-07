5189f15ff98f31592fc40e737fc04eee6a0123ce
Torendra Rasik <torendrarasik@gmail.com>
2024-06-07 10:56:29 -0400

    (client): Adds spotify widget to footer and fixes layout

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -37,6 +37,16 @@ const X_ICON = `
 <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 509.64"><rect width="512" height="509.64" rx="115.61" ry="115.61"/><path fill="#fff" fill-rule="nonzero" d="M323.74 148.35h36.12l-78.91 90.2 92.83 122.73h-72.69l-56.93-74.43-65.15 74.43h-36.14l84.4-96.47-89.05-116.46h74.53l51.46 68.04 59.53-68.04zm-12.68 191.31h20.02l-129.2-170.82H180.4l130.66 170.82z"/></svg>
 `;
 
+const SPOTIFY_ICON = `
+<svg viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Spotify-color</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Color-" transform="translate(-200.000000, -460.000000)" fill="#00DA5A"> <path d="M238.16,481.36 C230.48,476.8 217.64,476.32 210.32,478.6 C209.12,478.96 207.92,478.24 207.56,477.16 C207.2,475.96 207.92,474.76 209,474.4 C217.52,471.88 231.56,472.36 240.44,477.64 C241.52,478.24 241.88,479.68 241.28,480.76 C240.68,481.6 239.24,481.96 238.16,481.36 M237.92,488.08 C237.32,488.92 236.24,489.28 235.4,488.68 C228.92,484.72 219.08,483.52 211.52,485.92 C210.56,486.16 209.48,485.68 209.24,484.72 C209,483.76 209.48,482.68 210.44,482.44 C219.2,479.8 230,481.12 237.44,485.68 C238.16,486.04 238.52,487.24 237.92,488.08 M235.04,494.68 C234.56,495.4 233.72,495.64 233,495.16 C227.36,491.68 220.28,490.96 211.88,492.88 C211.04,493.12 210.32,492.52 210.08,491.8 C209.84,490.96 210.44,490.24 211.16,490 C220.28,487.96 228.2,488.8 234.44,492.64 C235.28,493 235.4,493.96 235.04,494.68 M224,460 C210.8,460 200,470.8 200,484 C200,497.2 210.8,508 224,508 C237.2,508 248,497.2 248,484 C248,470.8 237.32,460 224,460" id="Spotify"> </path> </g> </g> </g></svg>
+`;
+
+const spotifySongs: String[] = [
+  'Lose Yourself - Eminem',
+  'Dandelions - Ruth B',
+  'Lucid Dream - Topic',
+];
+
 export interface TileCard {
   title: string;
   subtitle: string;
@@ -55,6 +65,9 @@ export interface TileCard {
 })
 export class HomeComponent {
   columns: number = 7;
+
+  currentSpotifySong =
+    spotifySongs[Math.floor(Math.random() * spotifySongs.length)];
 
   tileCards: TileCard[] = [
     {
@@ -119,6 +132,13 @@ export class HomeComponent {
       'x',
       sanitizer.bypassSecurityTrustHtml(X_ICON)
     );
+
+    iconRegistry.addSvgIconLiteral(
+      'spotify',
+      sanitizer.bypassSecurityTrustHtml(SPOTIFY_ICON)
+    );
+
+    console.log(this.currentSpotifySong);
   }
 
   @HostListener('window:resize', []) updateColumns() {
