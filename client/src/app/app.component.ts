df0ac279f23778bbd462ddaf12ab812fa83267c1
Torendra Rasik <torendrarasik@gmail.com>
2024-06-09 16:56:36 -0400

    (client): Adds gsap to modules and background component foundation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { BackgroundComponent } from './background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, BackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
